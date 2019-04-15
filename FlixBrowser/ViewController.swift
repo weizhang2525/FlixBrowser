c454da5d962751bc6e4c77a397c37ef651841aa9
Wei Zhang <wjzhang1@uci.edu>
2019-04-14 21:44:11 -0700

    Initial Commit

---
+++ FlixBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixBrowser
+//
+//  Created by Wei Zhang on 4/14/19.
+//  Copyright © 2019 Wei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
