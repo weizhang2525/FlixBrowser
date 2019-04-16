5879dc398b30ee8a74db78dc9c75390379ac1da2
Wei Zhang <wjzhang1@uci.edu>
2019-04-16 11:10:06 -0700

    added tab bar and grid views for posters

---
+++ FlixBrowser/Views/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixBrowser
+//
+//  Created by Wei Zhang on 4/16/19.
+//  Copyright © 2019 Wei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
